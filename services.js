991a42806792a8ea5e7c2f1dc406675498cd312b
Prajwal Haniya <82883854+prajwalhaniya@users.noreply.github.com>
2024-01-04 23:03:46 +0530

    Add ability to read content from the pdf

--- services.js
+++ services.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { PDFDocument } = require('pdf-lib');
 const PDFParser = require('pdf2json');
+const jsonDiff = require('json-diff');
+const { PDFExtract } = require('pdf.js-extract');
 
 const pdf_1 = `pdfs/1.pdf`;
 const pdf_2 = `pdfs/2.pdf`;
@@ -50,25 +52,65 @@ const self = module.exports = {
     },
 
     getPdf1Content: () => new Promise((resolve, reject) => {
-        const pdfParser_1 = new PDFParser();
-        pdfParser_1.loadPDF(pdf_1);
-        pdfParser_1.on("data", page => resolve(page));
-    }),
+        // const pdfParser_1 = new PDFParser();
+        // pdfParser_1.loadPDF(pdf_1);
+        // pdfParser_1.on("pdfParser_dataReady", pdfTextData => {
+        //     const rawText = pdfTextData.Pages[0].Texts.map((text) =>
+        //     Buffer.from(text.R[0].T, 'base64').toString('utf-8'));
+        //    resolve(rawText);
+        // });
+        // pdfParser_1.on("data", page => resolve(page));
+        const pdfExtract = new PDFExtract();
+        const options = {}; /* see below */
+        pdfExtract.extract('pdfs/1.pdf', options, (err, data) => {
+        if (err) return console.log(err);
+            resolve(data);
+        });
+}),
 
     getPdf2Content: () => new Promise((resolve, reject) => {
-        const pdfParser_2 = new PDFParser();
-        pdfParser_2.loadPDF(pdf_2);
-        pdfParser_2.on("data", page => resolve(page));
+        // const pdfParser_2 = new PDFParser();
+        // pdfParser_2.loadPDF(pdf_2);
+        // pdfParser_2.on("data", page => resolve(page));
+        // pdfParser_2.on("pdfParser_dataReady", pdfTextData => {
+        //     const rawText = pdfTextData.Pages[0].Texts.map((text) =>
+        //     Buffer.from(text.R[0].T, 'base64').toString('utf-8'));
+        //    resolve(rawText);
+        // });
+        const pdfExtract = new PDFExtract();
+        const options = {};
+        pdfExtract.extract('pdfs/2.pdf', options, (err, data) => {
+        if (err) return console.log(err);
+            resolve(data);
+        });
     }),
 
     getContents:async () => {
         try {
             const pdf1_content = await self.getPdf1Content();
             const pdf2_content = await self.getPdf2Content();
-            console.log({ pdf1_content, pdf2_content });
+
+            const pagesOfPdf1 = pdf1_content.pages;
+            const countOfPagesOfPdf1 = pagesOfPdf1.length;
+            const contentsOfPdf1 = [];
+
+            const pagesOfPdf2 = pdf2_content.pages;
+            const countOfPagesOfPdf2 = pagesOfPdf2.length;
+            const contentsOfPdf2 = [];
+
+            for (let i = 0; i < countOfPagesOfPdf1; i++) {
+                contentsOfPdf1.push(...pagesOfPdf1[i].content);
+            };
+
+            for (let j = 0; j < countOfPagesOfPdf2; j++) {
+                contentsOfPdf2.push(...pagesOfPdf2[i].content);
+            }
+
+            // check the difference with string that is parsed
+           
         } catch (error) {
             console.log('Error while reading the contents of pdfs', error);
             return { success: false, message: 'Error occured while getting the contents of pdf' };
         }
-    }
+    },
 }
