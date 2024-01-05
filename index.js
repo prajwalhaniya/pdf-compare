b23458eaee9774a36c1d0361a827a87824f06c6f
Prajwal Haniya <82883854+prajwalhaniya@users.noreply.github.com>
2024-01-05 22:35:12 +0530

    clean the code of index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const app = require('express')();
 const PORT = 3005;
 
-const fs = require('fs');
-const services = require('./services');
-
-
-app.get('/', async (req, res) => {
-    const pdf_1 = `pdfs/1.pdf`;
-    const pdf_2 = `pdfs/2.pdf`;
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline');
-    res.send(fs.readFileSync(pdf_1));
-});
+const services = require('./services/compareServices');
 
 app.get('/compare-pdfs', async (req, res) => {
-    const pagesCount = await services.getPagesCount();
-    const metadata = await services.getMetadata();
-    const contents = await services.getContents();
-
-    const data = { pagesCount, metadata, contents };
+    const contents = await services.comparePdfs();
+    const data = { contents };
     res.json(data);
 })
 
